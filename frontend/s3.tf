485853ab6877bcd987f9966130059a1663128d5c
Brett Stephen <igadget777@gmail.com>
2022-05-23 12:45:59 -0400

    Rename aws_iam_policy_document

--- frontend/s3.tf
+++ frontend/s3.tf
@@ -95,7 +95,7 @@ resource "aws_s3_bucket_website_configuration" "redirect" {
 #######################
 resource "aws_s3_bucket_policy" "policy" {
   bucket = aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.cdn-oai-policy.json
+  policy = data.aws_iam_policy_document.bucket-policy.json
 }
 #  for now don't upload zip file //TODO
 # ######################################################
