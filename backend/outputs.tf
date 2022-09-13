40ba06b958cd7335c7ca08336fad34156b082f04
Brett Stephen <igadget777@gmail.com>
2022-09-13 17:43:23 -0400

    Add: acm certificate arn output

--- backend/outputs.tf
+++ backend/outputs.tf
@@ -6,3 +6,16 @@ output "deployment_invoke_url" {
   description = "Deployment invoke url"
   value       = aws_api_gateway_deployment.deployment.invoke_url
 }
+
+output "api_gw_domain_name" {
+  description = "API Gateway Domain Name"
+  value       = aws_api_gateway_domain_name.api-resume.domain_name
+}
+output "api_gw_regional_domain_name" {
+  description = "API Gateway Regional Domain Name"
+  value       = aws_api_gateway_domain_name.api-resume.regional_domain_name
+}
+output "api_gw_regional_zone_id" {
+  description = "API Gateway Regional Zone ID"
+  value       = aws_api_gateway_domain_name.api-resume.regional_zone_id
+}
