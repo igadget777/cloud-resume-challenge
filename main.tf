0b908f3c8efdbc39b9cb6aa3dec7f41f5064579c
Brett Stephen <igadget777@gmail.com>
2022-09-13 19:10:29 -0400

    Fix: format

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
     key    = "default/frontend-backend/terraform.tfstate"
 
     dynamodb_table = "terraform-locks-resume-challenge"
-    encrypt = true #ensures Terraform state will be encrypted on disk when stored in S3
+    encrypt        = true #ensures Terraform state will be encrypted on disk when stored in S3
   }
 }
 
