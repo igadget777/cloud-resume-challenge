845613fbabb24a1c915c9bc8f789118725899e57
Brett Stephen <igadget777@gmail.com>
2022-09-13 17:39:30 -0400

    Change: remove provider and backend

--- backend/main.tf
+++ backend/main.tf
@@ -1,20 +1,3 @@
 terraform {
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.13.0"
-    }
-  }
-  required_version = "1.1.8"
-
-  # Configure Terraform to store the state in your S3 bucket
-  backend "s3" {
-    region = "us-east-1"
-    bucket = "terraform-state-resume-challenge"
-    key    = "default/backend/terraform.tfstate"
-
-    # dynamodb_table = "terraform-locks-resume-challenge"
-    encrypt = true #ensures Terraform state will be encrypted on disk when stored in S3
-  }
+  required_version = ">=1.1.8"
 }
